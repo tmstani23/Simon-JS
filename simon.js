b154d6f5cc45654a1a4daf27c0b4f4ad200c633a
Tim Work Laptop <tmstani23@gmail.com>
2018-02-27 15:43:06 -0600

    Update 2
    Added functionality to user move function to verify that the user has input the correct button.  Began work on a delay for the generate sequence.

--- simon.js
+++ simon.js
@@ -4,7 +4,7 @@
 
 
 //Plays a sound for each part of the sequence and when user click corresponding button 
-//If user presses a wrong button they are notified and the same sequence plays again
+
 //User can see how many steps are in current sequence
 //User can click a reset button to start the game over with 1 sequence
 //Strict Mode: If the user makes an incorrect click they are notified and the game resets to 1 sequence
@@ -16,6 +16,15 @@ let USER_SEQ = "";
 let SEQ_STRING = "";
 let SEQ_LENGTH = 3;
 
+
+// let pausing = setTimeout(addDelay, 750);
+        
+// clearTimeout();
+
+
+function addDelay() {
+    console.log("adding 0.75 sec delay...");
+}
 //create 1 random sequence of n length:
 function genSequence(length) {
     //generate random number between 0 and 3:
@@ -24,9 +33,10 @@ function genSequence(length) {
     //console.log(randomInt);
     
     for(i=0; i<length; i++) {
+        //add 3/4 delay between each addition to the sequence:
+        
         let randomInt = Math.floor(Math.random() * Math.floor(4));
         SEQ_STRING += SEQ_ARR[randomInt];
-        
     }
     console.log(SEQ_STRING);
     return SEQ_STRING;
@@ -34,14 +44,35 @@ function genSequence(length) {
 
 //create +1 length escalating sequences up to 20:
 //create user move sequence with buttons
-function userMove(position) {
+function userMove(num) {
     
-    USER_SEQ += position;
-    console.log(USER_SEQ);
-    if(SEQ_LENGTH === USER_SEQ.length) {
-        verifySeq(USER_SEQ);
-    }
     
+    
+    
+    //currentIndex += 1;
+    USER_SEQ += num;
+    let currentIndex = USER_SEQ.length - 1;
+    
+    
+    //Synchronize user moves with current sequence index positions:
+    //console.log(USER_SEQ[currentIndex]);
+    //check num against last number of gen sequence string:
+        //get index position of number in user sequence
+        console.log(currentIndex);
+        //check same index position of seqstring
+        let genSeqVal = parseInt(SEQ_STRING[currentIndex]);    
+        console.log(genSeqVal);
+            //check if value at that index position = num
+            if(num === genSeqVal) {
+                console.log("numbers match" + num + genSeqVal);
+            }
+    //If user presses a wrong button they are notified and the same sequence plays again
+        if(num != genSeqVal) {
+            console.log("incorrect number" + num + genSeqVal);
+            //notify user:
+            //play current sequence:
+
+        }
 }
 
 //After user inputs correct sequence
