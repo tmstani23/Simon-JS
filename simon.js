070b8478823b059fe2a0c58adcd7cd0735cfb656
Tim Work Laptop <tmstani23@gmail.com>
2018-03-01 15:48:25 -0600

    Update 4
    Added sounds and play sound function.  Added display message function.  Added logic for blocking player moves while sequence is being generated/playing.  Not working.

--- simon.js
+++ simon.js
@@ -16,14 +16,20 @@ let USER_SEQ = "";
 let SEQ_STRING = "";
 let SEQ_LENGTH = 3;
 let ITER = 0;
+let USER_TURN = false;
 
+const SOUND_1 = new Audio('snds/simonSound1.mp3');
+const SOUND_2 = new Audio('snds/simonSound2.mp3');
+const SOUND_3 = new Audio('snds/simonSound3.mp3');
+const SOUND_4 = new Audio('snds/simonSound4.mp3');
+let SOUND_ARR = [SOUND_1, SOUND_2, SOUND_3, SOUND_4];
 
         
 // clearTimeout();
+//document.getElementById("button1").onmousedown = function() {playSound(0)}
 
-
-function addDelay() {
-    
+function playSeq() {
+    USER_TURN = false;
     setTimeout(function () {    
                   
         console.log(SEQ_STRING[ITER])
@@ -31,14 +37,16 @@ function addDelay() {
         ;                     
         //console.log(SEQ_STRING.length);
         if (ITER < SEQ_STRING.length) {            
-           addDelay();             
-        }                       
+           playSeq();             
+        }
+        //USER_TURN = true;                       
      }, 3000)
      
      //Reset iterator to 0 once it reaches sequence length
      if(ITER === SEQ_STRING.length) {
          ITER = 0;
      }
+    
 }
 //create 1 random sequence of n length:
 function genSequence(length) {
@@ -58,23 +66,34 @@ function genSequence(length) {
         //console.log("afterpause");
     }
     console.log(SEQ_STRING);
-    addDelay();
+    playSeq();
     //reset iterator to 0
     //ITER = 0;
     return SEQ_STRING;
 }
-
+function playSound(val) {
+    if(window.chrome) {
+        SOUND_ARR[val].load();
+        SOUND_ARR[val].play();
+    }
+    else {
+        SOUND_1.play();
+    }
+    //SOUND_1.pause();
+}
 //create +1 length escalating sequences up to 20:
 //create user move sequence with buttons
 function userMove(num) {
     
-    
-    
-    
+    if(!USER_TURN) {
+        displayMsg("Wait your turn");
+        return;
+    }
+    let sndIndex = num-1;
     //currentIndex += 1;
     USER_SEQ += num;
+    playSound(sndIndex);
     let currentIndex = USER_SEQ.length - 1;
-    
     
     //Synchronize user moves with current sequence index positions:
     //console.log(USER_SEQ[currentIndex]);
@@ -86,22 +105,28 @@ function userMove(num) {
         console.log(genSeqVal);
             //check if value at that index position = num
             if(num === genSeqVal) {
-                console.log("numbers match" + num + genSeqVal);
+                //console.log("numbers match" + num + genSeqVal);
             }
     //If user presses a wrong button they are notified and the same sequence plays again
         if(num != genSeqVal) {
             console.log("incorrect number" + num + genSeqVal);
             //notify user:
+            displayMsg("Incorrect Sequence");
+            //play error sound
             //play current sequence:
+            playSeq();
 
+        }
+        if(USER_SEQ.length === SEQ_STRING.length) {
+            verifySeq();
         }
 }
 
 //After user inputs correct sequence
     //Same sequence is repeated back with an additional step
 //create verify sequence function:
-function verifySeq(inputSeq) {
-    if(inputSeq.length === SEQ_STRING.length) {
+function verifySeq() {
+    
         
         if(USER_SEQ === SEQ_STRING) {
             console.log("string verified");
@@ -110,8 +135,12 @@ function verifySeq(inputSeq) {
             console.log("incorrect string");
         }
         
-    }
 }
+
+function displayMsg(msg) {
+    document.getElementById("errorP").innerHTML = msg;
+}
+
 
 genSequence(SEQ_LENGTH);
 // if(SEQ_LENGTH < 20) {
